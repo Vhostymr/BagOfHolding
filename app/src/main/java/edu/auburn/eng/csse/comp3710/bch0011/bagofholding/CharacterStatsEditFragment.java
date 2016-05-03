8b33e08ae910f01939754b00233cd3888efe6abd
Aaron <ascherer1993@gmail.com>
2016-05-03 14:21:36 -0500

    changed fragment and updated fragment java

--- app/src/main/java/edu/auburn/eng/csse/comp3710/bch0011/bagofholding/CharacterStatsEditFragment.java
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/bch0011/bagofholding/CharacterStatsEditFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 
 public class CharacterStatsEditFragment extends Fragment {
@@ -14,45 +15,67 @@ public class CharacterStatsEditFragment extends Fragment {
     MainActivity parentActivity;
 
     //Edit Text Fields
-    //Stats
-    EditText strengthET;
-    EditText dexterityET;
-    EditText constitutionET;
-    EditText intelligenceET;
-    EditText wisdomET;
-    EditText charismaET;
+
+        //Character Info
+        EditText nameET;
+        EditText levelET;
+        EditText experienceET;
+
+
+        //Stats
+        EditText strengthET;
+        EditText dexterityET;
+        EditText constitutionET;
+        EditText intelligenceET;
+        EditText wisdomET;
+        EditText charismaET;
+
+        //Secondary Stats
+        EditText armorClassET;
+        EditText initiativeET;
+        EditText speedET;
+        EditText maxHitPointsET;
+        EditText temporaryHitPointsET;
+
+    //Spinners
+        //CharacterInfo
+        Spinner alignmentSP;
+        Spinner raceSP;
+        Spinner classSP;
+
+
 
     //Check Boxes
-    //Proficiencies
-    CheckBox savingThrowsStrengthCB;
-    CheckBox athleticsStrengthCB;
+        //Proficiencies
+        CheckBox savingThrowsStrengthCB;
+        CheckBox athleticsStrengthCB;
 
-    CheckBox savingThrowsDexterityCB;
-    CheckBox acrobaticsDexterityCB;
-    CheckBox sleightOfHandDexterityCB;
-    CheckBox stealthDexterityCB;
+        CheckBox savingThrowsDexterityCB;
+        CheckBox acrobaticsDexterityCB;
+        CheckBox sleightOfHandDexterityCB;
+        CheckBox stealthDexterityCB;
 
-    CheckBox savingThrowsConstitutionCB;
+        CheckBox savingThrowsConstitutionCB;
 
-    CheckBox savingThrowsIntelligenceCB;
-    CheckBox arcanaIntelligenceCB;
-    CheckBox historyIntelligenceCB;
-    CheckBox investigationIntelligenceCB;
-    CheckBox natureIntelligenceCB;
-    CheckBox religionIntelligenceCB;
+        CheckBox savingThrowsIntelligenceCB;
+        CheckBox arcanaIntelligenceCB;
+        CheckBox historyIntelligenceCB;
+        CheckBox investigationIntelligenceCB;
+        CheckBox natureIntelligenceCB;
+        CheckBox religionIntelligenceCB;
 
-    CheckBox savingThrowsWisdomCB;
-    CheckBox animalHandlingWisdomCB;
-    CheckBox insightWisdomCB;
-    CheckBox medicineWisdomCB;
-    CheckBox perceptionWisdomCB;
-    CheckBox survivalWisdomCB;
+        CheckBox savingThrowsWisdomCB;
+        CheckBox animalHandlingWisdomCB;
+        CheckBox insightWisdomCB;
+        CheckBox medicineWisdomCB;
+        CheckBox perceptionWisdomCB;
+        CheckBox survivalWisdomCB;
 
-    CheckBox savingThrowsCharismaCB;
-    CheckBox deceptionCharismaCB;
-    CheckBox intimidationCharismaCB;
-    CheckBox performanceCharismaCB;
-    CheckBox persuasionCharismaCB;
+        CheckBox savingThrowsCharismaCB;
+        CheckBox deceptionCharismaCB;
+        CheckBox intimidationCharismaCB;
+        CheckBox performanceCharismaCB;
+        CheckBox persuasionCharismaCB;
 
     public CharacterStatsEditFragment() {
     }
@@ -73,6 +96,10 @@ public class CharacterStatsEditFragment extends Fragment {
         parentActivity = (MainActivity) getActivity();
         final View view = inflater.inflate(R.layout.fragment_character_edit, container, false);
 
+
+
+
+
         //EditTextSetters
         strengthET = (EditText) view.findViewById(R.id.et_strength);
         dexterityET = (EditText) view.findViewById(R.id.et_dexterity);
@@ -80,6 +107,19 @@ public class CharacterStatsEditFragment extends Fragment {
         intelligenceET = (EditText) view.findViewById(R.id.et_intelligence);
         wisdomET = (EditText) view.findViewById(R.id.et_wisdom);
         charismaET = (EditText) view.findViewById(R.id.et_charisma);
+        nameET = (EditText) view.findViewById(R.id.et_name);
+        levelET = (EditText) view.findViewById(R.id.et_level);
+        experienceET = (EditText) view.findViewById(R.id.et_experience_points);
+        armorClassET = (EditText) view.findViewById(R.id.et_armor_class);
+        initiativeET = (EditText) view.findViewById(R.id.et_initiative);
+        speedET = (EditText) view.findViewById(R.id.et_speed);
+        maxHitPointsET = (EditText) view.findViewById(R.id.et_max_hit_points);
+        temporaryHitPointsET = (EditText) view.findViewById(R.id.et_temporary_hit_points);
+        //SpinnerSetters
+        alignmentSP = (Spinner) view.findViewById(R.id.sp_alignment);
+        raceSP = (Spinner) view.findViewById(R.id.sp_race);
+        classSP = (Spinner) view.findViewById(R.id.sp_class);
+
 
         //CheckBoxSetters
         savingThrowsStrengthCB = (CheckBox) view.findViewById(R.id.cb_saving_throws_strength);
@@ -117,6 +157,13 @@ public class CharacterStatsEditFragment extends Fragment {
         return view;
     }
 
+    public String[] getBasicCharacterInfo(){
+        String[] basicInfo = new String[2];
+        basicInfo[0] = nameET.getText().toString();
+        basicInfo[1] = levelET.getText().toString();
+        return basicInfo;
+    }
+
     public Models.StatsModel getStatsModel(){
         return Models.setStatsModel(strengthET.getText().toString(), charismaET.getText().toString(),
                                                             constitutionET.getText().toString(), dexterityET.getText().toString(),
@@ -124,17 +171,21 @@ public class CharacterStatsEditFragment extends Fragment {
     }
     public Models.ProficiencyModel getProficiencyModel(){
         return Models.setProficiencyModel(savingThrowsStrengthCB.isChecked(), athleticsStrengthCB.isChecked(),
-                                                                              savingThrowsDexterityCB.isChecked(), acrobaticsDexterityCB.isChecked(),
-                                                                              sleightOfHandDexterityCB.isChecked(), stealthDexterityCB.isChecked(),
-                                                                              savingThrowsConstitutionCB.isChecked(), savingThrowsIntelligenceCB.isChecked(),
-                                                                              arcanaIntelligenceCB.isChecked(), historyIntelligenceCB.isChecked(),
-                                                                              investigationIntelligenceCB.isChecked(), natureIntelligenceCB.isChecked(),
-                                                                              religionIntelligenceCB.isChecked(), savingThrowsWisdomCB.isChecked(),
-                                                                              animalHandlingWisdomCB.isChecked(), insightWisdomCB.isChecked(),
-                                                                              medicineWisdomCB.isChecked(), perceptionWisdomCB.isChecked(),
-                                                                              survivalWisdomCB.isChecked(), savingThrowsCharismaCB.isChecked(),
-                                                                              deceptionCharismaCB.isChecked(), intimidationCharismaCB.isChecked(),
-                                                                              performanceCharismaCB.isChecked(), persuasionCharismaCB.isChecked());
+                savingThrowsDexterityCB.isChecked(), acrobaticsDexterityCB.isChecked(),
+                sleightOfHandDexterityCB.isChecked(), stealthDexterityCB.isChecked(),
+                savingThrowsConstitutionCB.isChecked(), savingThrowsIntelligenceCB.isChecked(),
+                arcanaIntelligenceCB.isChecked(), historyIntelligenceCB.isChecked(),
+                investigationIntelligenceCB.isChecked(), natureIntelligenceCB.isChecked(),
+                religionIntelligenceCB.isChecked(), savingThrowsWisdomCB.isChecked(),
+                animalHandlingWisdomCB.isChecked(), insightWisdomCB.isChecked(),
+                medicineWisdomCB.isChecked(), perceptionWisdomCB.isChecked(),
+                survivalWisdomCB.isChecked(), savingThrowsCharismaCB.isChecked(),
+                deceptionCharismaCB.isChecked(), intimidationCharismaCB.isChecked(),
+                performanceCharismaCB.isChecked(), persuasionCharismaCB.isChecked());
     }
-
+    public Models.SecondaryStatsModel getSecondaryStatsModel(){
+        return Models.setSecondaryStatsModel(armorClassET.getText().toString(),initiativeET.getText().toString(),
+                                             speedET.getText().toString(), maxHitPointsET.getText().toString(),
+                                             temporaryHitPointsET.getText().toString());
+    }
 }
